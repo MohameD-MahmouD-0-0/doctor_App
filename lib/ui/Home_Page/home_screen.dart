5bd65ae37c3c131880f26bbf64e544b36844d323
Electronica Care <mahamedmahmoud3880@gmail.com>
2025-09-04 16:18:48 +0300

    added notification photo. done with home screen app bar.

--- lib/ui/Home_Page/home_screen.dart
+++ lib/ui/Home_Page/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:doctor/ui/Home_Page/tap_home_page.dart';
+import 'package:doctor/ui/app_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -5,7 +7,20 @@ class HomeScreen extends StatelessWidget{
   static const String routeName = 'HomeScreen';
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(child:
+      Container(
+        margin: EdgeInsets.fromLTRB(20, 16, 20, 28),
+        width: double.infinity,
+        child:Column(
+          children: [
+            TapHomePage(),
+          ],
+        ) ,
+      )
+      ),
+    );
   }
 
 }
---
+++ lib/ui/Home_Page/tap_home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../app_color.dart';
+
+class TapHomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Hi, Omar',style: TextStyle(
+              fontSize: 18.sp,
+              fontWeight: FontWeight.w700,
+              color: Colors.black
+            ),),
+            Text('How Are you Today?',style: TextStyle(
+              fontSize: 13.sp,
+              fontWeight: FontWeight.w400,
+              color: AppColor.grey
+            ),),
+          ],
+        ),CircleAvatar(
+          radius: 20.sp,
+          backgroundColor: AppColor.grey.withAlpha(40),
+          child: Image.asset('assets/images/Button.png'),
+        )
+      ],
+    );
+  }
+
+}
